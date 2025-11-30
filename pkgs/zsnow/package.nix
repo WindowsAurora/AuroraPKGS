43af0660f4cd57452402bdfd29baaa7373d043e8
WindowsAurora <104532354+WindowsAurora@users.noreply.github.com>
2025-11-30 21:57:38 +0100

    I HATE MY LIFE AND I WANT TO DIE.

--- pkgs/zsnow/package.nix
+++ pkgs/zsnow/package.nix
@@ -1,38 +1,38 @@
 # pkgs/zsnow/package.nix
 { lib
-, stdenv
 , fetchFromGitHub
-, zig
-, pkg-config
 , wayland
 , wayland-protocols
 , wlr-protocols
-, libxkbcommon # Dependency of zig-wayland
+, buildZigPackage
+, pkg-config
+, libxkbcommon
 }:
 
-stdenv.mkDerivation {
+buildZigPackage {
   pname = "zsnow";
   version = "unstable-2025-11-30";
 
   src = fetchFromGitHub {
     owner = "WindowsAurora";
-    repo = "ZSNoW";
+    repo = "ZSnoW";
     rev = "56c15a924de4a0d386324eaca5ffabedb97255d3";
     hash = "sha256-vtnHWgJVJfePdtPr7onYp+Ztq4cuNSjyuuDuipyv6oE=";
   };
 
-  # nativeBuildInputs: All tools and libraries needed AT BUILD TIME.
-  # The hook handles Zig dependencies. pkg-config needs all .pc files.
+  buildMode = "ReleaseSafe";
+
+  # nativeBuildInputs: Tools and libraries needed for the build.
+  # This makes 'wayland-scanner' and all .pc files available to zig build.
   nativeBuildInputs = [
-    zig.hook
     pkg-config
-    wayland           # Provides wayland-scanner and wayland-scanner.pc
-    wayland-protocols # Provides its .pc files
-    wlr-protocols     # Provides its .pc files
-    libxkbcommon         # Provides its .pc files
+    wayland
+    wayland-protocols
+    wlr-protocols
+    libxkbcommon
   ];
 
-  # buildInputs: All libraries needed AT RUN TIME by the final binary.
+  # buildInputs: Libraries the final program needs to run.
   buildInputs = [
     wayland
     wayland-protocols
@@ -40,30 +40,10 @@ stdenv.mkDerivation {
     libxkbcommon
   ];
 
-  # This creates the `zon.hash` file that the zig.hook requires.
-  # The hash is taken DIRECTLY from the lock file you provided.
-  postPatch = ''
-    echo "sha256-EMQM3ch2xSBhLxb6a2hUy8WONCuAVWM8jtJIkh2GtA0=" > zon.hash
-  '';
-
-  # Standard build and install phases. The hook configures the rest.
-  buildPhase = ''
-    runHook preBuild
-    zig build -Doptimize=ReleaseSafe
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp zig-out/bin/zsnow $out/bin/
-    runHook postInstall
-  '';
-
   meta = with lib; {
     description = "A basic XSnow clone for wayland written in zig";
-    homepage = "https://github.com/WindowsAurora/ZSNoW";
-    license = licenses.cc-by-nc-sa-40; # This will be handled by the build command flags.
+    homepage = "https://github.com/WindowsAurora/ZSnoW";
+    license = licenses.cc-by-nc-sa-40;
     platforms = platforms.linux;
     mainProgram = "zsnow";
   };
