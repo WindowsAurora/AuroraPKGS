b817f1fe8c7c632f5d15d0cfcab74f7968a00a09
WindowsAurora <104532354+WindowsAurora@users.noreply.github.com>
2025-11-30 19:32:41 +0100

    .....

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           inherit system;
           config.allowUnfree = true; # Required for steam
         };
-        buildZigPackage = (zig2nix.zig-env.${system} { inherit pkgs; }).package;
+        buildZigPackage = (zig2nix.zig-env.${system} { nixpkgs = pkgs; }).package;
       in
       {
         packages = {
